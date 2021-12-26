eeaf1a77474fd7eeae5b862afed8f9f9cd269831
nikkialistra <nikkialistra@gmail.com>
2021-12-26 14:10:29 +0500

    Add preview displaying

--- Assets/Kernel/UI/Game/InfoPanelView.cs
+++ Assets/Kernel/UI/Game/InfoPanelView.cs
@@ -8,10 +8,17 @@ namespace Kernel.UI.Game
     [RequireComponent(typeof(UIDocument))]
     public class InfoPanelView : MonoBehaviour
     {
+        [SerializeField] private Texture2D _travelerPreview;
+        [SerializeField] private Texture2D _lumberjackPreview;
+        [SerializeField] private Texture2D _masonPreview;
+        [SerializeField] private Texture2D _meleePreview;
+        [SerializeField] private Texture2D _archerPreview;
+
         private VisualElement _tree;
 
         private VisualElement _infoPanel;
-        
+        private VisualElement _infoImage;
+
         private Label _type;
         private Label _name;
         private ProgressBar _health;
@@ -23,6 +30,7 @@ namespace Kernel.UI.Game
             _tree = GetComponent<UIDocument>().rootVisualElement;
 
             _infoPanel = _tree.Q<VisualElement>("info-panel");
+            _infoImage = _tree.Q<VisualElement>("info-image");
 
             _type = _tree.Q<Label>("nomination__type");
             _name = _tree.Q<Label>("nomination__name");
@@ -44,6 +52,25 @@ namespace Kernel.UI.Game
         private void FillIn()
         {
             _infoPanel.RemoveFromClassList("not-displayed");
+            FillInPreview();
+            FillInProperties();
+        }
+
+        private void FillInPreview()
+        {
+            _infoImage.style.backgroundImage = _unit.UnitType switch
+            {
+                UnitType.Traveler => new StyleBackground(_travelerPreview),
+                UnitType.Lumberjack => new StyleBackground(_lumberjackPreview),
+                UnitType.Mason => new StyleBackground(_masonPreview),
+                UnitType.Melee => new StyleBackground(_meleePreview),
+                UnitType.Archer => new StyleBackground(_archerPreview),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        private void FillInProperties()
+        {
             _type.text = _unit.UnitType.ToString();
             _name.text = _unit.Name;
             _health.value = _unit.Health;

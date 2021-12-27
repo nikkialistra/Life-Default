0f3bb0ccda3c5a015668a33b32a05af10e6c4da9
nikkialistra <nikkialistra@gmail.com>
2021-12-27 21:06:25 +0500

    Update ui selection system to use ui tookit

--- Assets/Kernel/Selection/SelectionArea.cs
+++ Assets/Kernel/Selection/SelectionArea.cs
@@ -1,71 +1,40 @@
-using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Kernel.Selection
 {
-    [RequireComponent(typeof(Canvas))]
+    [RequireComponent(typeof(UIDocument))]
     public class SelectionArea : MonoBehaviour
     {
-        [Title("Area")]
-        [Required]
-        [SerializeField] private RectTransform _selectionArea;
-        
-        [Title("Borders")]
-        [Required]
-        [SerializeField] private GameObject _bordersParent;
-        [Space]
-        [Required]
-        [SerializeField] private RectTransform _topRect;
-        [Required]
-        [SerializeField] private RectTransform _leftRect;
-        [Required]
-        [SerializeField] private RectTransform _rightRect;
-        [Required]
-        [SerializeField] private RectTransform _bottomRect;
+        private VisualElement _root;
+        private VisualElement _selection;
+        private float _scale;
 
-        private Canvas _canvas;
-
-        private GameObject _selectionGameObject;
-        
         private void Awake()
         {
-            _canvas = GetComponent<Canvas>();
-            _selectionGameObject = _selectionArea.gameObject;
+            var uiDocument = GetComponent<UIDocument>();
+            _root = uiDocument.rootVisualElement;
+            _scale = (float) uiDocument.panelSettings.referenceResolution.x / Screen.width;
+            
+            _selection = _root.Q<VisualElement>("selection");
         }
 
         public void Draw(Rect rect)
         {
-            if (_selectionGameObject.activeSelf == false)
+            if (_selection.ClassListContains("not-displayed"))
             {
-                _selectionGameObject.SetActive(true);
-                _bordersParent.SetActive(true);
+                _selection.RemoveFromClassList("not-displayed");
             }
-
-            _selectionArea.position = rect.center;
-            _selectionArea.sizeDelta = rect.size / _canvas.scaleFactor;
-
-            DrawBorders(rect);
-        }
-
-        private void DrawBorders(Rect rect)
-        {
-            _topRect.position = new Vector2(rect.xMin + rect.width / 2, rect.yMax);
-            _topRect.sizeDelta = new Vector2(rect.width / _canvas.scaleFactor, 1);
             
-            _leftRect.position = new Vector2(rect.xMin, rect.yMin + rect.height / 2);
-            _leftRect.sizeDelta = new Vector2(1, rect.height / _canvas.scaleFactor);
-
-            _rightRect.position = new Vector2(rect.xMax, rect.yMin + rect.height / 2);
-            _rightRect.sizeDelta = new Vector2(1, rect.height / _canvas.scaleFactor);
-
-            _bottomRect.position = new Vector2(rect.xMin + rect.width / 2, rect.yMin);
-            _bottomRect.sizeDelta = new Vector2(rect.width / _canvas.scaleFactor, 1);
+            _selection.style.left = rect.xMin * _scale;
+            _selection.style.bottom = rect.yMin * _scale;
+            _selection.style.width = rect.width * _scale;
+            _selection.style.height = rect.height * _scale;
         }
 
         public void StopDrawing()
         {
-            _selectionGameObject.SetActive(false);
-            _bordersParent.SetActive(false);
+            _selection.AddToClassList("not-displayed");
         }
     }
 }

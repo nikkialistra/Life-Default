dfef1cb6b279c9de63d15197679c7ed5924bcb75
nikkialistra <nikkialistra@gmail.com>
2021-12-30 14:53:59 +0500

    Make names conform to each other along the project

--- Assets/Game/Units/Scripts/UnitTypes/UnitTypeAppearance.cs
+++ Assets/Game/Units/Scripts/UnitTypes/UnitTypeAppearance.cs
@@ -6,16 +6,16 @@ namespace Game.Units.UnitTypes
     [CreateAssetMenu(fileName = "UnitTypeAppearance", menuName = "Data/Unit Type Appearance")]
     public class UnitTypeAppearance : ScriptableObject
     {
+        [Title("Required Skin")]
+        [Required]
+        [SerializeField] private Material _skin;
+        
         [Title("Optional Accessories")]
         [SerializeField] private GameObject _headEndAccessory;
         [SerializeField] private GameObject _headAccessory;
         [SerializeField] private GameObject _rightHandAccessory;
         [SerializeField] private GameObject _upperChestAccessory;
 
-        [Title("Required Skin")]
-        [Required]
-        [SerializeField] private Material _skin;
-        
         public GameObject HeadEndAccessory => _headEndAccessory;
         public GameObject HeadAccessory => _headAccessory;
         public GameObject RightHandAccessory => _rightHandAccessory;

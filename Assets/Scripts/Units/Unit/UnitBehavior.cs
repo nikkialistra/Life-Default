225da46467e7c181cbc4176b89352834c081c98c
nikkialistra <nikkialistra@gmail.com>
2022-01-15 19:22:07 +0500

    Add NPBehave plugin

---
+++ Assets/Scripts/Units/Unit/UnitBehavior.cs
@@ -0,0 +1,26 @@
+﻿using NPBehave;
+using UnityEngine;
+
+namespace Units.Unit
+{
+    public class UnitBehavior : MonoBehaviour
+    {
+        private Root _behaviorTree;
+
+        private void Start()
+        {
+            ConstructBehaviorTree();
+            _behaviorTree.Start();
+        }
+
+        private void ConstructBehaviorTree()
+        {
+            _behaviorTree = new Root(
+                new Sequence(
+                    new Action(() => Debug.Log("Hello World!")),
+                    new WaitUntilStopped()
+                )
+            );
+        }
+    }
+}
